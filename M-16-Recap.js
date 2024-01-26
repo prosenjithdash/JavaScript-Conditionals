0ebdb6e1a55edc99c10daefb2237f6de780a31eb
prosenjithdashpappu <prosenjith.edu@gmail.com>
2024-01-26 23:35:43 +0600

    Recap completed

--- M-16-Recap.js
+++ M-16-Recap.js
@@ -1,1 +1,41 @@
 // Variables
+
+/*
+
+Variable ==> vary + able or Container and also possible change of values.
+
+5 thing of variables
+
+var appleBox = 5 ;
+
+*** Numerical Variable ***
+
+var number = 45 ;
+
+*** String Variable ***
+
+var name = 'Pappu';
+
+*** Boolean Variable ***
+
+var cseStudent = true;
+
+*** NAMING CONVENTION ***
+1. Single word
+2. No  quote
+3. No gap or Dash
+4. Can not start with number
+5. Can not be a keyword
+
+
+isCseStudent ==>
+IsCseStudent ==>
+is__Cse_Student ==>
+
+
+*** OPARETION ***
+
+1. + , - , * , / , %
+2. += , -= , *= , /= , %=
+3. 
+*/
